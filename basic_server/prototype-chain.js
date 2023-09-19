1c142cb6b0841601d451549242c372c614bbe9d4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-19 19:42:23 +0200

    feat: simple inheritance

--- basic_server/prototype-chain.js
+++ basic_server/prototype-chain.js
@@ -18,3 +18,20 @@ pepe.saluda();
 console.log(Persona.prototype);
 console.log(Object.getPrototypeOf(pepe));
 console.log(Persona.prototype === Object.getPrototypeOf(pepe));
+
+// Herencia simple
+function Agente(nombre) {
+  // heredar el constructor
+  // heredar las propiedades de las personas
+
+  Persona.call(this, nombre);
+}
+
+// Agente.prototype = Object.create(Persona.prototype);
+// Agente.prototype.constructor = Agente;
+
+Object.setPrototypeOf(Agente.prototype, Persona.prototype);
+
+const smith = new Agente('Smith');
+
+// Herencia multiple
