b0fc6b4ea799ae95db84bb8da77e28d81e48099f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 20:29:29 +0200

    feat: views variables and conditionals

--- nodeapp/routes/index.js
+++ nodeapp/routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.locals.texto = 'Hola';
+  res.locals.nombre = "<script>alert('inyeccion de codigo js')</script>";
+
+  const ahora = new Date();
+  res.locals.esPar = ahora.getSeconds() % 2 === 0;
+  res.locals.segundoActual = ahora.getSeconds();
+
   res.render('index', { title: 'Express' });
 });
 
