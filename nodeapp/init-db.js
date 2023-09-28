448a6157de7768b287784e611a506bb4d6dbb3ea
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-28 19:32:55 +0200

    feat: db-init script

---
+++ nodeapp/init-db.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const connection = require('./lib/connectMongoose');
+const Agente = require('./models/Agente');
+
+const initAgentes = async () => {
+  try {
+    // borrar todos los documentos de la coleccion de agentes
+    const deleted = await Agente.deleteMany({});
+    console.log(`Eliminados ${deleted.deletedCount} agentes`);
+    const inserted = await Agente.insertMany([
+      {
+        name: 'Smith',
+        age: 33,
+      },
+      {
+        name: 'Jones',
+        age: 23,
+      },
+      {
+        name: 'Brown',
+        age: 46,
+      },
+    ]);
+
+    console.log(`Created ${inserted.length} agentes`);
+  } catch (error) {
+    console.log('error init agentes', error);
+  }
+};
+
+const main = async () => {
+  try {
+    //inicializar la coleccion de agentes
+    await initAgentes();
+  } catch (error) {
+    console.log('Hubo un error', error);
+  }
+};
+
+main();
