e236fc057fda38ccc99bd734ad539c146a07f438
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-11 22:14:08 +0100

    feat: use bootstrap template

--- backend-nodejs-mongodb-main/nodeapp/routes/index.js
+++ backend-nodejs-mongodb-main/nodeapp/routes/index.js
@@ -2,19 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
-  res.locals.texto = 'Hola';
-  res.locals.nombre = '<script>alert("inyección de código")</script>'
+router.get('/', function (req, res, next) {
+  res.locals.texto = res.__('Hola');
+  res.locals.nombre = '<script>alert("inyección de código")</script>';
 
   const ahora = new Date();
-  res.locals.esPar = (ahora.getSeconds() % 2) === 0;
+  res.locals.esPar = ahora.getSeconds() % 2 === 0;
   res.locals.segundoActual = ahora.getSeconds();
 
   res.locals.usuarios = [
     { nombre: 'Smith', edad: 32 },
-    { nombre: 'Jones', edad: 27 }
-  ]
+    { nombre: 'Jones', edad: 27 },
+  ];
 
   res.render('index');
 });
@@ -34,16 +33,18 @@ router.get('/parametro_opcional/:numero?', (req, res, next) => {
 });
 
 // GET /producto/pantalones/talla/34/color/azul
-router.get('/producto/:nombre/talla/:talla([0-9]+)/color/:color', (req, res, next) => {
-  console.log(req.params)
+router.get(
+  '/producto/:nombre/talla/:talla([0-9]+)/color/:color',
+  (req, res, next) => {
+    console.log(req.params);
 
-  // const nombre = req.params.nombre;
-  // const talla = req.params.talla;
-  // const color = req.params.color;
-  const { nombre, talla, color } = req.params;
+    // const nombre = req.params.nombre;
+    // const talla = req.params.talla;
+    // const color = req.params.color;
+    const { nombre, talla, color } = req.params;
 
-  res.send(`Me pediste ${nombre} talla ${talla} color ${color}`);
-});
-
+    res.send(`Me pediste ${nombre} talla ${talla} color ${color}`);
+  }
+);
 
 module.exports = router;
