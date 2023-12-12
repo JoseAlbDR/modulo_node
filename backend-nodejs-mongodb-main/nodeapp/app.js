e51bb07cbf020cb1b541934e1017a69437259b04
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-12 20:19:40 +0100

    feat: login form

--- backend-nodejs-mongodb-main/nodeapp/app.js
+++ backend-nodejs-mongodb-main/nodeapp/app.js
@@ -46,6 +46,7 @@ app.use('/', require('./routes/index'));
 app.use('/features', require('./routes/features'));
 app.use('/users', require('./routes/users'));
 app.use('/change-locale', require('./routes/change-locale'));
+app.use('/login', require('./routes/auth'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ backend-nodejs-mongodb-main/nodeapp/controllers/LoginController.js
@@ -0,0 +1,7 @@
+class LoginController {
+  static index(req, res, next) {
+    res.render('login');
+  }
+}
+
+module.exports = LoginController;
