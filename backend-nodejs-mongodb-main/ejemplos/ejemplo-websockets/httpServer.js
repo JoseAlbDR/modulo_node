de149c693ab61bc1b1d244ff6b88ae0e061639f1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-12-19 20:27:31 +0100

    feat: create server for webSocketsServer module

--- backend-nodejs-mongodb-main/ejemplos/ejemplo-websockets/httpServer.js
+++ backend-nodejs-mongodb-main/ejemplos/ejemplo-websockets/httpServer.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const path = require('node:path');
+const http = require('http');
+const webSocketsServer = require('./webSocketsServer');
 
 const app = express();
 
@@ -9,6 +11,10 @@ app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3001, () => {
+const server = http.createServer(app);
+
+server.listen(3001, () => {
   console.log('Server listening on port 3000');
 });
+
+webSocketsServer(server);
